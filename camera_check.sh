05a11f4c238a4f012272cb87d6aae379508114d8
kris <kjg9190@gmail.com>
2022-10-08 14:56:02 +0200

    updating locally

---
+++ camera_check.sh
@@ -0,0 +1,3 @@
+#!bin/bash
+# run camera preview on visible monitor. requires desktop interface
+ve_sattelkar/bin/python3 hello_camera.py
---
+++ run_sattelkar.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+# script to run sattelkar monitor program
+echo hello world
+/home/pi/sattelkar/ve_sattelkar/bin/python3 --version
+
